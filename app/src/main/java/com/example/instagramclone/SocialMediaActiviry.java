23a3b3c58fd5f696a7d70ce277ef4d24911e95cd
vishwasranjan <vishwasranjan16@gmail.com>
2020-09-15 13:11:31 +0530

    Tab Layout Setup

--- app/src/main/java/com/example/instagramclone/SocialMediaActiviry.java
+++ app/src/main/java/com/example/instagramclone/SocialMediaActiviry.java
@@ -1,14 +1,33 @@
 package com.example.instagramclone;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+
 public class SocialMediaActiviry extends AppCompatActivity {
+    private Toolbar toolbar;
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
+    private TabAdapter tabAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_social_media_activiry);
+        setTitle("Social Media App");
+        toolbar=findViewById(R.id.mytoolbar);
+        setSupportActionBar(toolbar);
+
+        viewPager=findViewById(R.id.viewpager);
+        tabLayout=findViewById(R.id.tablayout);
+        tabAdapter=new TabAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(tabAdapter);
+        tabLayout.setupWithViewPager(viewPager);
+
     }
 }
